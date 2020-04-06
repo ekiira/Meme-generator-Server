ffd6606de816eb36a1a7030a4074188341f7fba9
Tosin Jimoh <jayoluwatosin@gmail.com>
2020-04-06 14:40:25 +0100

    tests

--- test/app.test.js
+++ test/app.test.js
@@ -1,14 +1,10 @@
-/* eslint-disable linebreak-style */
 /* eslint-disable no-undef */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
 
 const chai = require('chai');
 
 const { expect } = chai;
 const request = require('supertest');
 const app = require('../app');
-
 
 it('Main page content', async () => {
   request(app)
@@ -18,25 +14,25 @@ it('Main page content', async () => {
     });
 });
 
-it('Get memes', async () => {
-  request(app)
-    .get('/view')
-    .end((err, res) => {
-      expect(res.statusType).to.eq(2);
-      expect(res.status).to.eq(200);
-      // expect(res.text).to.have.lengthOf(360)
-    });
-});
+// it('Get memes', async () => {
+//   request(app)
+//     .get('/view')
+//     .end((err, res) => {
+//       expect(res.statusType).to.eq(2);
+//       expect(res.status).to.eq(200);
+//       // expect(res.text).to.have.lengthOf(360)
+//     });
+// });
 
-it('Create Memes', async () => {
-  request(app)
-    .post('/add')
-    .send({
-      caption: 'is it working',
-      imageUrl: 'someurl',
-      imageUploag: '',
-    })
-    .end((err, res) => {
-      expect(res.status).to.eq(200);
-    });
-});
+// it('Create Memes', async () => {
+//   request(app)
+//     .post('/add')
+//     .send({
+//       caption: 'is it working',
+//       imageUrl: 'someurl',
+//       imageUploag: '',
+//     })
+//     .end((err, res) => {
+//       expect(res.status).to.eq(200);
+//     });
+// });
